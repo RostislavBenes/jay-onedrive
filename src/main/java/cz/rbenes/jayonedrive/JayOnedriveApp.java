37c990b87abc24575b23232b9b6c36020a0c19f5
Rostislav Benes <benes.rostislav@gmail.com>
2018-01-10 20:10:55 +0100

    configuration of Spring boot.

--- src/main/java/cz/rbenes/jayonedrive/JayOnedriveApp.java
+++ src/main/java/cz/rbenes/jayonedrive/JayOnedriveApp.java
@@ -1,7 +1,12 @@
 package cz.rbenes.jayonedrive;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class JayOnedriveApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JayOnedriveApp.class);
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module cz.rbenes.jayonedrive {
     requires spring.boot.autoconfigure;
+    requires spring.boot;
 }
